83a5f0b0545efc9dd23b8699420006f48f2583d8
George Danila <geo.danila89@gmail.com>
2023-12-03 23:38:23 +0200

    Completed day 1

--- day_1.fsx
+++ day_1.fsx
@@ -1,16 +1,37 @@
 open System.IO
 open System
+open System.Text.RegularExpressions
 
 let readInput () =
     let inputPath = Path.Combine(__SOURCE_DIRECTORY__, "data" , "input_1.txt")    
     File.ReadAllLines(inputPath)
 
-let findDigit (line: seq<char>) =
-    line |> Seq.find (fun c -> Char.IsDigit(c)) |> string |> int
+let digitRegex = @"(\d|one|two|three|four|five|six|seven|eight|nine)"
+
+let getDigitValue (digit: string) =
+    match digit.ToLower() with
+    | "one" -> 1
+    | "two" -> 2
+    | "three" -> 3
+    | "four" -> 4
+    | "five" -> 5
+    | "six" -> 6
+    | "seven" -> 7
+    | "eight" -> 8
+    | "nine" -> 9
+    | _ -> digit |> int
+
+let findDigit (line: string) =    
+    let digit = Regex.Match(line, digitRegex).Value
+    getDigitValue digit    
+
+let findLastDigit (line: string) =
+    let lastDigit = Regex.Match(line, digitRegex, RegexOptions.RightToLeft).Value
+    getDigitValue lastDigit
 
 let getFirstAndLastDigits (line: string) : (int * int) =
     let firstDigit = findDigit line    
-    let lastDigit = findDigit (line |> Seq.rev)
+    let lastDigit = findLastDigit line
     (firstDigit, lastDigit)
 
 let solution = 
@@ -20,3 +41,4 @@ let solution =
     |> Seq.sum
 
 printfn "Solution: %d" solution
+

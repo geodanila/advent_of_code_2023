2778a5061f98720ba3c68006cee6e57ff69e4f16
George Danila <geo.danila89@gmail.com>
2023-12-04 08:14:55 +0200

    Cleaned up day2

--- day_2.fsx
+++ day_2.fsx
@@ -2,24 +2,21 @@ open System.IO
 open System.Text.RegularExpressions
 type Game = { id: int; red: int; green: int; blue: int }
 
-let parseGame (line: string): Game =
-    let idRegex = "Game (\d+):"
-    let redRegex = "(\d+) red"
-    let greenRegex = "(\d+) green"
-    let blueRegex = "(\d+) blue"
+let idRegex = "Game (\d+):"
+let redRegex = "(\d+) red"
+let greenRegex = "(\d+) green"
+let blueRegex = "(\d+) blue"
+
+let extractMaxValue line regex =
+    Regex.Matches(line, regex)
+    |> Seq.map (fun x -> x.Groups.[1].Value |> int)
+    |> Seq.max
+
+let parseGame (line: string): Game =    
     let id = Regex.Match(line, idRegex).Groups.[1].Value |> int
-    let red = 
-        Regex.Matches(line, redRegex)
-        |> Seq.map (fun x -> x.Groups.[1].Value |> int)
-        |> Seq.max
-    let green = 
-        Regex.Matches(line, greenRegex)
-        |> Seq.map (fun x -> x.Groups.[1].Value |> int)
-        |> Seq.max
-    let blue =
-        Regex.Matches(line, blueRegex)
-        |> Seq.map (fun x -> x.Groups.[1].Value |> int)
-        |> Seq.max
+    let red = extractMaxValue line redRegex
+    let green = extractMaxValue line greenRegex
+    let blue = extractMaxValue line blueRegex
     { id = id; red = red; green = green; blue = blue}
 
 let readInput () =

ed87ce7f9162e1d4aab896b8ca8d618498289aaf
George Danila <geo.danila89@gmail.com>
2023-12-04 15:41:07 +0200

    Using Sets for intersection

--- day_4.fsx
+++ day_4.fsx
@@ -5,9 +5,10 @@ open System.Collections.Generic
 
 type Card = {
     CardNumber: int
-    WinningNumbers: int array
-    MyNumbers: int array
-}
+    WinningNumbers: int Set
+    MyNumbers: int Set
+} with 
+    member this.Wins = (Set.intersect this.WinningNumbers this.MyNumbers).Count
 
 let split (sep: string) (toSplit: string) =
     toSplit.Split([|sep|], StringSplitOptions.RemoveEmptyEntries)
@@ -27,27 +28,25 @@ let parseCard (line: string) =
         |> (fun x -> x[1].Trim())
         |> split " "
         |> Array.map int
+        |> Set.ofArray
 
     let myNumbers =
         splitCard[1] 
         |> split " "
         |> Array.map int
-    { CardNumber = cardNb; WinningNumbers = winningNumbers; MyNumbers = myNumbers }
+        |> Set.ofArray
 
-let getMatches card =
-    card.WinningNumbers     
-    |> Array.filter (fun x -> card.MyNumbers |> Array.contains x)
+    { CardNumber = cardNb; WinningNumbers = winningNumbers; MyNumbers = myNumbers }    
 
 let rec computeScoreRec matches score =
     match matches with
     | 0 -> score
     | _ -> computeScoreRec (matches - 1) (score * 2)
 
-let computeScore card =
-    let nbOfMatches = getMatches card |> Array.length
-    match nbOfMatches with
+let computeScore (card: Card) =    
+    match card.Wins with
     | 0 -> 0
-    | _ -> computeScoreRec (nbOfMatches - 1) 1
+    | _ -> computeScoreRec (card.Wins - 1) 1
 
 // Part 1
 let cards = 
@@ -61,11 +60,17 @@ let total =
     |> List.sumBy snd
 printfn "Part1: %d" total
 
+let takeOrAll count list =
+    let length = List.length list
+    if length >= count 
+        then list |> List.take count
+        else list
+
 // Part 2
 let processTotal () =
     let mutable counters = Dictionary<int, int>()
     let maxNb = cards |> Seq.map _.CardNumber |> Seq.max
-    
+        
     for i in 1 .. maxNb do
         counters.Add(i, 0)
 
@@ -74,7 +79,7 @@ let processTotal () =
         | head :: tail -> 
             counters[head.CardNumber] <- counters[head.CardNumber] + 1
                 
-            let winnings = getMatches head |> Array.length
+            let winnings = head.Wins
             let copies = 
                 if winnings > 0 then
                     let remainingCards = cards |> List.skipWhile (fun x -> x.CardNumber <= head.CardNumber)
@@ -83,11 +88,12 @@ let processTotal () =
                         else remainingCards
                 else []
             
-            processInternal copies
             processInternal tail
-        | _ -> ()            
+            processInternal copies            
+        | [] -> ()
 
-    processInternal cards
+    cards 
+    |> processInternal
 
     counters 
     |> Seq.map (fun x -> x.Value)

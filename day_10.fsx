dc1248b74fb61d4af0098f742e602fd248efed0c
George Danila <geo.danila89@gmail.com>
2023-12-10 22:39:35 +0200

    Day 10 - Part 2

--- day_10.fsx
+++ day_10.fsx
@@ -35,6 +35,44 @@ module Samples =
     LJ.LJ
     """
 
+    let sample5 = """
+    ...........
+    .S-------7.
+    .|F-----7|.
+    .||.....||.
+    .||.....||.
+    .|L-7.F-J|.
+    .|..|.|..|.
+    .L--J.L--J.
+    ...........
+    """
+
+    let sample6 = """
+    .F----7F7F7F7F-7....
+    .|F--7||||||||FJ....
+    .||.FJ||||||||L7....
+    FJL7L7LJLJ||LJ.L-7..
+    L--J.L7...LJS7F-7L7.
+    ....F-J..F7FJ|L7L7L7
+    ....L7.F7||L7|.L7L7|
+    .....|FJLJ|FJ|F7|.LJ
+    ....FJL-7.||.||||...
+    ....L---J.LJ.LJLJ...
+    """
+
+    let sample7 = """
+    FF7FSF7F7F7F7F7F---7
+    L|LJ||||||||||||F--J
+    FL-7LJLJ||||||LJL-77
+    F--JF--7||LJLJ7F7FJ-
+    L---JF-JLJ.||-FJLJJ7
+    |F|F-JF---7F7-L7L|7|
+    |FFJF7L7F-JF7|JL---7
+    7-L-JL7||F7|L7F-7F7|
+    L.L7LFJ|||||FJL7||LJ
+    L7JLJL-JLJLJL--JLJ.L
+    """
+
 let readSampleInput input = 
     input
     |> String.splitTrim "\n" 
@@ -86,21 +124,22 @@ let parseTiles (input: char array array): Tiles =
             tiles[row, col] <- tile
     tiles
 
-let tilesToString (tiles: Tiles) =
-    let builder = StringBuilder()
-    for i in 0..tiles.GetLength(0) - 1 do
-        for j in 0..tiles.GetLength(1) - 1 do
-            match tiles[i, j] with
-            | Empty -> builder.Append "." |> ignore
-            | Start -> builder.Append "S" |> ignore
-            | Pipe Vertical -> builder.Append "║" |> ignore
-            | Pipe Horizontal -> builder.Append "═" |> ignore
-            | Pipe NE -> builder.Append "╚" |> ignore
-            | Pipe NW -> builder.Append "╝" |> ignore
-            | Pipe SW -> builder.Append "╗" |> ignore
-            | Pipe SE -> builder.Append "╔" |> ignore
-        builder.Append("\n") |> ignore
-    builder.ToString()
+module Tiles =
+    let toString (tiles: Tiles) =
+        let builder = StringBuilder()
+        for i in 0..tiles.GetLength(0) - 1 do
+            for j in 0..tiles.GetLength(1) - 1 do
+                match tiles[i, j] with
+                | Empty -> builder.Append "." |> ignore
+                | Start -> builder.Append "S" |> ignore
+                | Pipe Vertical -> builder.Append "║" |> ignore
+                | Pipe Horizontal -> builder.Append "═" |> ignore
+                | Pipe NE -> builder.Append "╚" |> ignore
+                | Pipe NW -> builder.Append "╝" |> ignore
+                | Pipe SW -> builder.Append "╗" |> ignore
+                | Pipe SE -> builder.Append "╔" |> ignore
+            builder.Append("\n") |> ignore
+        builder.ToString()
 
 let checkNextTile (tileFrom: Tile) (tileTo: Tile) direction = 
     //printfn "checkNextTile: %A -> %A in direction %A" tileFrom tileTo direction
@@ -152,42 +191,43 @@ let findPath (tiles: Tiles) =
 
     let start = 
         match tiles |> Array2D.tryFindLocation (fun x -> x = Start) with
-        | None -> failwithf "Could not find starting position in tiles \n%s" (tilesToString tiles)
+        | None -> failwithf "Could not find starting position in tiles \n%s" (Tiles.toString tiles)
         | Some (i, j) -> 
-            printfn "Starting position is %i, %i for tiles \n%s" i j (tilesToString tiles)
+            printfn "Starting position is %i, %i for tiles \n%s" i j (Tiles.toString tiles)
             (i, j)
 
-    let circuit = HashSet<_>()
+    let visited = HashSet<_>()
     let mutable current = start
     let mutable shouldCheckNext = true
 
     while shouldCheckNext do
         let i, j = current        
-        circuit.Add(i, j) |> ignore
+        visited.Add(i, j) |> ignore
 
         //printfn "Currently at %A (%i, %i)" tiles[i,j] i j
 
-        if isValidPosition tiles i (j + 1) circuit && checkNextPosition tiles i j i (j + 1) Right then
+        if isValidPosition tiles i (j + 1) visited && checkNextPosition tiles i j i (j + 1) Right then
             //printfn "Moving from tile %A (%i, %i) to %A (%i, %i) in direction %A" tiles[i, j] i j tiles[i, j + 1] i (j + 1) Right
             current <- (i, j + 1)
-        else if isValidPosition tiles i (j - 1) circuit && checkNextPosition tiles i j i (j - 1) Left then
+        else if isValidPosition tiles i (j - 1) visited && checkNextPosition tiles i j i (j - 1) Left then
             //printfn "Moving from tile %A (%i, %i) to %A (%i, %i) in direction %A" tiles[i, j] i j tiles[i, j - 1] i (j - 1) Left
             current <- (i, j - 1)
-        else if isValidPosition tiles (i + 1) j circuit && checkNextPosition tiles i j (i + 1) j Down then
+        else if isValidPosition tiles (i + 1) j visited && checkNextPosition tiles i j (i + 1) j Down then
             //printfn "Moving from tile %A (%i, %i) to %A (%i, %i) in direction %A" tiles[i, j] i j tiles[i + 1, j] (i + 1) j Down
             current <- (i + 1, j)
-        else if isValidPosition tiles (i - 1) j circuit && checkNextPosition tiles i j (i - 1) j Up then
+        else if isValidPosition tiles (i - 1) j visited && checkNextPosition tiles i j (i - 1) j Up then
             //printfn "Moving from tile %A (%i, %i) to %A (%i, %i) in direction %A" tiles[i, j] i j tiles[i - 1, j] (i - 1) j Up
             current <- (i - 1, j)
         shouldCheckNext <- current <> start
 
-    circuit |> Seq.toList
+    visited |> List.ofSeq
 
-let path =
+let tiles = 
+    //readSampleInput Samples.sample7
     readInput ()
     |> parseTiles
-    |> findPath
 
+let path = tiles |> findPath
 path |> printfn "Path is: %A"
 
 let pathWithDistances =
@@ -198,8 +238,7 @@ let pathWithDistances =
             if index > path.Length / 2 
             then path.Length - index 
             else index
-        (position, distance)
-    )
+        (position, distance))
 
 let maxDistance = 
     pathWithDistances
@@ -207,3 +246,26 @@ let maxDistance =
     |> snd
 
 printfn "Part1: %i" maxDistance
+
+// Part 2...
+let countEnclosedTiles (tiles: Tiles) (path: (int * int) list) =
+    let maxRow = tiles.GetLength(0) - 1
+    let maxCol = tiles.GetLength(1) - 1
+    let mutable count = 0
+
+    for i in 0..maxRow do
+        let mutable above = false
+        for j in 0..maxCol do
+            match tiles[i, j], path |> Seq.contains (i, j) with 
+            | (Pipe Vertical | Pipe NE | Pipe NW), true -> 
+                above <- not above
+            | _ -> ()
+
+            if above && not (path |> Seq.contains (i, j)) then
+                count <- count + 1
+    count
+
+let nbOfEnclosedTiles =
+    countEnclosedTiles tiles path
+
+printfn "Part2: %i" nbOfEnclosedTiles

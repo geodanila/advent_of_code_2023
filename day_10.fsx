c5f9247c480448f5a2f4cc06dac90a2a626eeace
George Danila <geo.danila89@gmail.com>
2023-12-10 22:49:23 +0200

    Cleaned up debug code

--- day_10.fsx
+++ day_10.fsx
@@ -2,82 +2,6 @@
 open System.Text
 open System.Collections.Generic
 
-module Samples =
-    let sample1 = """
-    .....
-    .S-7.
-    .|.|.
-    .L-J.
-    .....
-    """
-
-    let sample2 = """
-    -L|F7
-    7S-7|
-    L|7||
-    -L-J|
-    L|-JF
-    """
-
-    let sample3 = """
-    ..F7.
-    .FJ|.
-    SJ.L7
-    |F--J
-    LJ...
-    """
-
-    let sample4 = """
-    7-F7-
-    .FJ|7
-    SJLL7
-    |F--J
-    LJ.LJ
-    """
-
-    let sample5 = """
-    ...........
-    .S-------7.
-    .|F-----7|.
-    .||.....||.
-    .||.....||.
-    .|L-7.F-J|.
-    .|..|.|..|.
-    .L--J.L--J.
-    ...........
-    """
-
-    let sample6 = """
-    .F----7F7F7F7F-7....
-    .|F--7||||||||FJ....
-    .||.FJ||||||||L7....
-    FJL7L7LJLJ||LJ.L-7..
-    L--J.L7...LJS7F-7L7.
-    ....F-J..F7FJ|L7L7L7
-    ....L7.F7||L7|.L7L7|
-    .....|FJLJ|FJ|F7|.LJ
-    ....FJL-7.||.||||...
-    ....L---J.LJ.LJLJ...
-    """
-
-    let sample7 = """
-    FF7FSF7F7F7F7F7F---7
-    L|LJ||||||||||||F--J
-    FL-7LJLJ||||||LJL-77
-    F--JF--7||LJLJ7F7FJ-
-    L---JF-JLJ.||-FJLJJ7
-    |F|F-JF---7F7-L7L|7|
-    |FFJF7L7F-JF7|JL---7
-    7-L-JL7||F7|L7F-7F7|
-    L.L7LFJ|||||FJL7||LJ
-    L7JLJL-JLJLJL--JLJ.L
-    """
-
-let readSampleInput input = 
-    input
-    |> String.splitTrim "\n" 
-    |> Array.map _.ToCharArray()
-
 let readInput () =
     Input.readAllLines "input_10.txt"
     |> Array.map _.ToCharArray()
@@ -223,7 +147,6 @@ let findPath (tiles: Tiles) =
     visited |> List.ofSeq
 
 let tiles = 
-    //readSampleInput Samples.sample7
     readInput ()
     |> parseTiles
 
